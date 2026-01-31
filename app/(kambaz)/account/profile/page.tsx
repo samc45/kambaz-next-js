6a1de6f3e9be8b17bfa1f160ca1c081ea65e5537
samc45 <cohen.sam@northeastern.edu>
2026-01-31 17:26:51 -0500

    profile page cleanup

--- app/(kambaz)/account/profile/page.tsx
+++ app/(kambaz)/account/profile/page.tsx
@@ -1,23 +1,27 @@
 import Link from "next/link";
-import { Card, Form, FormControl } from "react-bootstrap";
+import { Card, FormControl } from "react-bootstrap";
 import { IoIosLogOut } from "react-icons/io";
 export default function Profile() {
   return (
     <div id="wd-profile-screen" className="w-50 mx-auto mt-5">
       <Card className="w-75 p-5 mb-3 d-flex align-items-center gap-2">
         <h3>Profile</h3>
-        <FormControl defaultValue="alice" placeholder="username" className="wd-username" /><br />
-        <FormControl defaultValue="123" placeholder="password" type="password"
-          className="wd-password" /><br />
-        <FormControl defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br />
-        <FormControl defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br />
-        <FormControl defaultValue="2000-01-01" type="date" id="wd-dob" /><br />
-        <FormControl defaultValue="alice@wonderland" type="email" id="wd-email" /><br />
+        <FormControl defaultValue="alice" placeholder="username" className="wd-username" />
+        <FormControl defaultValue="123" placeholder="password" type="password" className="wd-password" />
+        <FormControl defaultValue="Alice" placeholder="First Name" id="wd-firstname" />
+        <FormControl defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" />
+        <FormControl defaultValue="2000-01-01" type="date" id="wd-dob" />
+        <FormControl defaultValue="alice@wonderland" type="email" id="wd-email" />
         <FormControl as="select" defaultValue="FACULTY" id="wd-role">
-          <option value="USER">User</option>       <option value="ADMIN">Admin</option>
-          <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
+          <option value="USER">User</option>
+          <option value="ADMIN">Admin</option>
+          <option value="FACULTY">Faculty</option>
+          <option value="STUDENT">Student</option>
         </FormControl>
-        <Link href="signin" className="d-flex flex-row align-items-center mt-4" > <IoIosLogOut className="me-1" /> Sign Out </Link>
+        <Link href="signin" className="d-flex flex-row align-items-center mt-4" >
+          <IoIosLogOut className="me-1" />
+          Sign Out
+        </Link>
       </Card>
     </div>
   );
